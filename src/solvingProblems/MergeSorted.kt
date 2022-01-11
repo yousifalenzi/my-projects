0662826a9c5685553fbce5ce562fd05f961d6a3b
hitmix <moonty4790@gmail.com>
2022-01-11 03:45:15 +0300

    Initial commit

--- src/solvingProblems/MergeSorted.kt
+++ src/solvingProblems/MergeSorted.kt
@@ -1,6 +1,6 @@
 package solvingProblems
 
-import sortAndsearch_Algorthm.binarySearch.Binary_khanway
+import JustTry.Higher_Order_Function_and_Lambda.printindex
 
 //merge sorted Tow arrays
 
@@ -8,14 +8,14 @@ import sortAndsearch_Algorthm.binarySearch.Binary_khanway
 
  fun merge( merge: IntArray): IntArray {
 
-       // [2,4,6,8],[3,5,9,10]
 
 
 
 
-    for (i in 1 until merge.size) {// [5,2,6,3,1]
-        val dataSaveAtIndexI = merge[i] //2
-        var current = i - 1 //0
+
+    for (i in 1 until merge.size) {
+        val dataSaveAtIndexI = merge[i]
+        var current = i - 1
 
 
         while (current >= 0 && merge[current] > dataSaveAtIndexI) {
@@ -35,7 +35,7 @@ import sortAndsearch_Algorthm.binarySearch.Binary_khanway
 }
 
 
-//save element at index i [2,5,6,3,1]}
+
 
 
 
@@ -46,63 +46,57 @@ import sortAndsearch_Algorthm.binarySearch.Binary_khanway
 
 fun makeMerge(array1:Array<Int>,array2:Array<Int>): IntArray { // brute force
       //merge arrays size
-       val sizeTotal = array1.size+array2.size
-       val NewArray:IntArray= IntArray(sizeTotal)
+        val sizeTotal = array1.size+array2.size
+        val NewArray:IntArray= IntArray(sizeTotal)
+        var i = 0
+        var j = 0
+        var index= 0
+        var indexsave = 0
+        var arr :Array<Int>?=null
 
 
 
+           while (i<=array1.size-1&&j<=array2.size-1){
 
 
-    //operations
-    var LastItem_array1 =array1[array1.size-1]
-    var LastItem_array2 =array2[array2.size-1]
-    var While_Index=sizeTotal-1 // 9
-    var i_array1 =array1.size-2
-    var j_array2 =array2.size-2
+                  if (array1[i]<array2[j]){
+
+                      NewArray[index] = array1[i]
+                      i++
+                      arr= array2
+                      indexsave = j
+                  }
+                  else{
+                      NewArray[index] = array2[j]
+                      j++
+                      arr= array1
+                      indexsave = i
+                  }
+                   index++
+              }
+
+            //add what items remaining
+           for (ii in indexsave until arr!!.size){
+
+               NewArray[index] = arr[ii]
+               index++
+
+           }
 
 
-    while (While_Index>0&&(!(i_array1 == -1 && j_array2 == -1))){
-
-          if (LastItem_array1>LastItem_array2){
-              NewArray[While_Index] = LastItem_array1
-              While_Index--
-              LastItem_array1=array1[i_array1]
-              i_array1--
-          }
-
-          else{
-              NewArray[While_Index] = LastItem_array2
-              While_Index--
-              LastItem_array2=array2[j_array2]
-              j_array2--
-          } }
-
-    if (array1[i_array1+1]<array2[j_array2+1]){
-
-        NewArray[i_array1+1]=array1[i_array1+1]
-        NewArray[i_array1+2]=array2[j_array2+1]
-    }
-    else{
-
-        NewArray[i_array1+1]=array2[i_array1+1]
-        NewArray[i_array1+2]=array1[j_array2+1]
-    }
-
-   return NewArray
+    return NewArray
 }
 fun main(){
 
 
-      val intArray1 = arrayOf(5,8,9,12,15,17)
-      val intArray2 = arrayOf(0,7)
+      val intArray1 = arrayOf(4,31,32,33,55,77,88)
+      val intArray2 = arrayOf(2,3,5)
 
     val makeMerge = makeMerge(intArray1, intArray2)
-    val binary = Binary_khanway()
 
-    val applyBinarysearch = binary.apply_BinarySearch(5, makeMerge)
 
     makeMerge.forEach { print(" $it ") }
-   print("\n we found it in index $applyBinarysearch")
+
 
 
 }
