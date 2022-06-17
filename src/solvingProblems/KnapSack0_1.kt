9f22f4fc1fbbf45113277aba31f9f5a381d0c416
hitmix <moonty4790@gmail.com>
2022-06-17 18:43:55 +0300

    this me i did it

--- src/solvingProblems/KnapSack0_1.kt
+++ src/solvingProblems/KnapSack0_1.kt
@@ -1,43 +1,44 @@
 package solvingProblems
 
- private var totalwightandProfit = Array<String?>(2) { "" }
-    fun getAll():Array<String?> = totalwightandProfit
-   fun KnapSack0_1(w:Array<Int>,p:Array<Int>,KnapSack_size:Int):Array<IntArray>{
+    private var Total = Array<String?>(2) { "" }
+    fun getAll():Array<String?> = Total
+    fun knapsack(w:Array<Int>,p:Array<Int>,knapsack_size:Int):Array<IntArray>{
 
-    val Matrix = Array(w.size) { IntArray(KnapSack_size+1) }
+    val matrix = Array(w.size) { IntArray(knapsack_size+1) }
 
     for (i in w.indices){//1 -- 4
 
       var wight = 0
 
-      while (wight<=KnapSack_size){
+      while (wight<=knapsack_size){
 
         if (i==0||wight==0){
 
-          Matrix[i][wight] = 0
+          matrix[i][wight] = 0
         }
         else if (w[i]<=wight)
         {
 
-          Matrix[i][wight] = max( p[i]+Matrix[i-1][wight-w[i]],Matrix[i-1][wight])
+          matrix[i][wight] = max( p[i]+matrix[i-1][wight-w[i]],matrix[i-1][wight])
         }
         else {
 
-          Matrix[i][wight] = Matrix[i-1][wight]
+          matrix[i][wight] = matrix[i-1][wight]
         }
 
         wight++
 
       } } 
    
-   return  Matrix
+   return  matrix
    }
-   fun IncludedOrNot(Bag:Array<IntArray>,w:Array<Int>,p:Array<Int>): Array<String?> {
+    fun includedOrNot(Bag:Array<IntArray>, w:Array<Int>, p:Array<Int>): Array<String?> {
+
 
        var row = Bag.size-1 // 4
        var column = Bag[0].size-1 //8
-       val arrayOfObjects = Array<String?>(Bag.size,{ "NotIncluded(0)"})
-       var wi = 0
+       val arrayOfObjects = Array<String?>(Bag.size) { "NotIncluded(0)" }
+     var wi = 0
        var pr = 0
      while (row>0&&column>0){
 
@@ -60,22 +61,25 @@ package solvingProblems
       }
 
 
-     totalwightandProfit[0]="All weight can be carried =${wi}"
-     totalwightandProfit[1]="All profit can be carried =${pr}"
+     Total[0]="All weight can be carried =${wi}"
+     Total[1]="All profit can be carried =${pr}"
 
       return arrayOfObjects
 
 
    }
-   private fun max(v1: Int,v2:Int):Int=if (v1>v2) v1 else v2
+    private fun max(v1: Int,v2:Int):Int=if (v1>v2) v1 else v2
 
 
 fun main (){
 
+  val wi = arrayOf(0, 2, 3, 4, 5)
+  val pr = arrayOf(0, 1, 2, 5, 6)
 
-  val knapsack01 = KnapSack0_1(arrayOf(0, 2, 3, 4, 5), arrayOf(0, 1, 2, 5, 6), 13)
 
-  val includedOrNot = IncludedOrNot(knapsack01, arrayOf(0, 2, 3, 4, 5),arrayOf(0, 1, 2, 5, 6))
+  val knapsack01 = knapsack(wi,pr, 2)
+
+  val includedOrNot = includedOrNot(knapsack01, wi,pr)
 
   for (i in includedOrNot.iterator()){
 
